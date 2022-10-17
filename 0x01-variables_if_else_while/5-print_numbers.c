1adba37a6b7d1e900b27c412309ac0570961c9c3
Kagiso <dolokag@gmail.com>
2022-10-16 23:05:07 -0700

    Print numbers 0 -9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit base 10 numbers.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar(number + '0');
+	putchar('\n');
+	return (0);
+}
