78a4b4a8d8ef56f2225df91a83d744a9af7da805
Kagiso <dolokag@gmail.com>
2022-10-19 12:52:02 -0700

    reverting back to nested for loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,14 @@
 
 void print_alphabet_x10(void)
 {
+	char alphabet;
 	int i;
 
 	for (i = 1; i <= 10; i++)
 	{
-		print_alphabet();
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+
+		_putchar('\n');
 	}
 }
