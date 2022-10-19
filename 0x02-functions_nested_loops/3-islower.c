1357615cbb7b420451ca071685318fc08f71d213
Kagiso <dolokag@gmail.com>
2022-10-19 13:24:55 -0700

    using an add instead of or

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _islower - a function that checks for lowercase character.
- *
+ * 
  * Return: 1 if c is lowercase. 0 otherwise.
  */
 
 int _islower(int c)
 {
-	if (c >= 97 || c <= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
