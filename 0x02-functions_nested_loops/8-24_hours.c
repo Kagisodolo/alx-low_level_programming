ca0afaceffd82f9353f14b9d0766bbfe7090f03c
Kagiso <dolokag@gmail.com>
2022-10-19 14:52:53 -0700

    fixing compile issues

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,13 +12,13 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for minute =0; minute <=59; minute++)
+		for (minute =0; minute <=59; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((mionute / 10) + '0');
-			_putchar((minue % 10) + '0');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
 	}
