481bfcb090504f744eadaf3f4f92a1edcc8e4fec
Kagiso <dolokag@gmail.com>
2022-10-21 11:47:05 -0700

    fnc that multiplies 2 ints

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers.
+ * @a: input integer 1.
+ * @b: input integer 2.
+ * Return: The result of multiplication if success. Error otherwise.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
